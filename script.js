61d70b0a96d90eeef03ce9206f90fb04b3f266f6
Marly Monsef <92717935+marlymonsefseif@users.noreply.github.com>
2025-01-14 02:19:32 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+function validationForm(event) {
+    event.preventDefault();
+    var isValid = true;
+    var errorIcon = document.querySelector(".errorIcon");
+    var errorFName = document.getElementById("errorFName");
+    var fName = document.getElementById("fname").value.trim();
+    if(fName == ""){
+        errorFName.textContent = "First Name cannot be empty";
+        errorIcon.style.visibility = "visible";
+        isValid = false;
+    }
+    else {
+        errorFName.textContent = "";
+    }
+
+    var errorLName = document.getElementById("errorLName");
+    var lName = document.getElementById("lname").value.trim();
+    if(lName == ""){
+        errorLName.textContent = "Last Name cannot be empty";
+        errorIcon.style.visibility = "visible";
+        isValid = false;
+    }
+    else {
+        errorLName.textContent = "";
+    }
+
+    var errorEmail = document.getElementById("errorEmail");
+    var email = document.getElementById("email").value.trim();
+    if(email == ""){
+        errorEmail.textContent = "Email cannot be empty";
+        errorIcon.style.visibility = "visible";
+        isValid = false;
+    }
+    else {
+        errorEmail.textContent = "";
+        if(validateEmail(email)) {
+            errorEmail.textContent = "";
+        }
+        else {
+            errorEmail.textContent = "Looks like this is not an email";
+            errorIcon.style.visibility = "visible";
+            isValid = false;
+        }
+    }
+
+    var errorPass = document.getElementById("errorPass");
+    var password = document.getElementById("pass").value.trim();
+    if(password == "") {
+        errorPass.textContent = "Password cannot be empty";
+        errorIcon.style.visibility = "visible";
+        isValid = false;
+    }
+    else if(password.length < 8) {
+        errorPass.textContent = "Password must be at least 8 characters";
+        errorIcon.style.visibility = "visible";
+        isValid = false;
+    }
+    else {
+        errorPass.textContent = "";
+    }
+
+    if(isValid){
+        form.submit();   
+    }
+}
+
+var form = document.getElementById("form");
+form.addEventListener("submit", validationForm);
+
+function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
